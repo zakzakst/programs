dc6678e9e7f9ff7c38aea2386eb5e9c4e994c883
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-06-17 00:04:18 +0900

    initial commit

---
+++ src/01/main.ts
@@ -0,0 +1,5 @@
+import './scss/style.scss';
+import './pug/index.pug';
+import { test } from './ts/index';
+
+test();
---
+++ webpack.config.js
@@ -0,0 +1,113 @@
+// モジュールの読み込み
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozjpeg = require('imagemin-mozjpeg');
+
+// 変数の設定
+const PROJECT_DIR = '01';
+const MODE = 'production';
+const enabledSourceMap = MODE === 'development';
+
+// 設定
+module.exports = {
+  mode: MODE,
+  entry: `./src/${PROJECT_DIR}/main.ts`,
+  output: {
+    path: `${__dirname}/docs/${PROJECT_DIR}`,
+    filename: 'script.js',
+  },
+
+  module: {
+    rules: [
+      // TypeScript用
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+      // Scss用
+      {
+        test: /\.scss/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: enabledSourceMap,
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: enabledSourceMap
+            },
+          },
+        ],
+      },
+      // Pug用
+      {
+        test: /\.pug$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].html',
+            },
+          },
+          'extract-loader',
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: `${__dirname}/src/${PROJECT_DIR}/img/`,
+          to: `${__dirname}/docs/${PROJECT_DIR}/img/[name][ext]`,
+        },
+      ],
+    }),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      plugins: [
+        ImageminMozjpeg({
+          quality: 85,
+          progressive: true,
+        }),
+      ],
+      pngquant: {
+        quality: '70-85',
+      },
+      gifsicle: {
+        interlaced: false,
+        optimizationLevel: 10,
+        colors: 256,
+      },
+      svgo: {},
+    }),
+  ],
+
+  resolve: {
+    extensions: [
+      '.ts', '.js',
+    ],
+  },
+};
